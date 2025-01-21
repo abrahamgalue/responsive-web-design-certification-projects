dd3ebe80fe0e4948584798588c8c533b666b6d5e
Abraham Galue <abrahamgalue512@gmail.com>
2025-01-21 10:23:16 -0400

    Initialized files to use tests

---
+++ tests/tribute-page/render.test.js
@@ -0,0 +1,14 @@
+// @ts-check
+import { test, expect } from '@playwright/test'
+
+test('has title', async ({ page }) => {
+  await page.goto('https://tribute-page-abraham.netlify.app/')
+
+  await expect(page).toHaveTitle(/Tribute Page/)
+})
+
+test('has header', async ({ page }) => {
+  await page.goto('https://tribute-page-abraham.netlify.app/')
+
+  await expect(page.getByRole('heading', { name: 'Michael Jackson' })).toBeVisible()
+})
