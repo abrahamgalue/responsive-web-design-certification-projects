dc4a28feab30570d59d0010c385d47d28d1848b0
Abraham Galue <abrahamgalue512@gmail.com>
2025-02-01 12:30:18 -0400

    Added tests to verify header visibility and navigation links

--- tests/product-landing-page/render.test.js
+++ tests/product-landing-page/render.test.js
@@ -8,3 +8,29 @@ test('has title', async ({ page }) => {
 
   await expect(page).toHaveTitle(/Product Landing Page/)
 })
+
+test('has header', async ({ page }) => {
+  await page.goto(URL)
+
+  await expect(page.locator('#header')).toBeVisible()
+  await expect(page.getByRole('link', { name: 'header logo shure' })).toBeVisible()
+  await expect(page.getByRole('link', { name: 'Features' })).toBeVisible()
+  await expect(page.getByRole('link', { name: 'Images' })).toBeVisible()
+  await expect(page.getByRole('link', { name: 'How It Works' })).toBeVisible()
+  await expect(page.getByRole('link', { name: 'Pricing' })).toBeVisible()
+})
+
+test('has logo', async ({ page }) => {
+  await page.goto(URL)
+
+  await expect(page.getByRole('link', { name: 'header logo shure' })).toBeVisible()
+})
+
+test('has navigation links', async ({ page }) => {
+  await page.goto(URL)
+
+  await expect(page.getByRole('link', { name: 'Features' })).toBeVisible()
+  await expect(page.getByRole('link', { name: 'Images' })).toBeVisible()
+  await expect(page.getByRole('link', { name: 'How It Works' })).toBeVisible()
+  await expect(page.getByRole('link', { name: 'Pricing' })).toBeVisible()
+})
