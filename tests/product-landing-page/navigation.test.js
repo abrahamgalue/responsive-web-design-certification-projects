4f88f58c5d43eb105e784dc722ef2dcf3c214af5
Abraham Galue <abrahamgalue512@gmail.com>
2025-02-04 10:50:18 -0400

    Added internal navigation test for product landing page

--- tests/product-landing-page/navigation.test.js
+++ tests/product-landing-page/navigation.test.js
@@ -14,3 +14,21 @@ test('can navigate to original page', async ({ page }) => {
 
   await expect(originalPage.url()).toContain('www.shure.com')
 })
+
+test('can navigate through the page', async ({ page }) => {
+  await page.goto(URL)
+
+  await page.getByRole('link', { name: 'Features' }).click()
+  await expect(page.getByRole('heading', { name: 'Your Vocal MVP' })).toBeVisible()
+
+  await page.getByRole('link', { name: 'Images' }).click()
+  await expect(page.locator('figure').filter({ hasText: 'How Vocals Should Sound' }).getByRole('img')).toBeVisible()
+
+  await page.getByRole('link', { name: 'How It Works' }).click()
+  await expect(page.locator('iframe[title="YouTube video player"]').contentFrame().locator('.ytp-cued-thumbnail-overlay-image')).toBeVisible()
+
+  await page.getByRole('link', { name: 'Pricing' }).click()
+
+  await expect(page.getByText('384.99')).toBeVisible()
+  await expect(page.getByText('528.99')).toBeVisible()
+})
